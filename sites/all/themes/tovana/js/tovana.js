37b0a21b93f9801c094560e472b5dc94c38a9e17
bruria1 <bruria@tikke.co.il>
2015-10-25 11:19:19 +0200

    user page

--- sites/all/themes/tovana/js/tovana.js
+++ sites/all/themes/tovana/js/tovana.js
@@ -41,7 +41,7 @@ $(".menu-button").click(function(){
   }
 });
 
-$( ".page-user .tabs-primary" ).prepend( "<h2 class='block__title block-title block-title-user'><span>User Menu</span></h2>" );
+$( ".page-user .tabs-primary" ).prepend( "<h2 class='block__title block-title block-title-user en'><span>User Menu</span></h2><h2 class='block__title block-title block-title-user he'><span>תפריט משתמש</span></h2>" );
 
 
 }
