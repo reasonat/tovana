682b079828a60fbd3b1b736da6d1ac823ad9e605
bruria1 <bruria@tikke.co.il>
2015-11-08 13:09:57 +0200

    node--event

--- sites/all/themes/tovana/templates/node--event.tpl.php
+++ sites/all/themes/tovana/templates/node--event.tpl.php
@@ -57,19 +57,3 @@
   <?php print render($content['comments']); ?>
 
 </article>
-
- <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js"></script> 
- <div title="Add to Calendar" class="addeventatc">
-    Add to Calendar
-    <span class="start">11/15/2015 09:00 AM</span>
-    <span class="end">11/15/2015 11:00 AM</span>
-    <span class="timezone">Europe/Paris</span>
-    <span class="title">Summary of the event</span>
-    <span class="description">Description of the event<br>Example of a new line</span>
-    <span class="location">Location of the event</span>
-    <span class="organizer">Organizer</span>
-    <span class="organizer_email">Organizer e-mail</span>
-    <span class="facebook_event">https://www.facebook.com/events/703782616363133</span>
-    <span class="all_day_event">false</span>
-    <span class="date_format">MM/DD/YYYY</span>
-</div> 
